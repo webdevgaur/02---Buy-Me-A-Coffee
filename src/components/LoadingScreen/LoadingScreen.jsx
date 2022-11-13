9c33cc3134af7a297ba69980cf045d71f31e044f
Anurag Gaur <anuraggaur495@gmail.com>
2022-11-14 01:20:31 +0530

    confetti effect

--- src/components/LoadingScreen/LoadingScreen.jsx
+++ src/components/LoadingScreen/LoadingScreen.jsx
@@ -3,9 +3,26 @@ import './LoadingScreen.css';
 import funnyGif from '../../assets/coffee3.gif';
 import loaderGif from '../../assets/loader.gif';
 import { useState } from 'react';
+import ConfettiGenerator from "confetti-js";
+import { useEffect } from 'react';
 
 function LoadingScreen({isWalletOpen, isTxnUnderway, setIsDialogVisible}) {
     const [isLoaded, setIsLoaded] = useState(false);
+    const [isConfettiVisible, setIsConfettiVisible] = useState(false);
+
+    useEffect(() => {
+        if (isConfettiVisible) {
+            const confettiSettings = { target: 'canvas',
+            clock: 50,
+            max: 200,
+            start_from_edge: true,
+            };
+            const confetti = new ConfettiGenerator(confettiSettings);
+            confetti.render();
+        }
+    }, [isConfettiVisible])
+    
+    
 
     const DialogLoader = () => {
         const closeDialog = () => {
@@ -13,11 +30,12 @@ function LoadingScreen({isWalletOpen, isTxnUnderway, setIsDialogVisible}) {
         }
         return (
             <>
-                <div className="coffee-plate"></div>
+                <canvas id='canvas' className="coffee-plate"></canvas>
                 {
                     isTxnUnderway ? 
                     <TxnLoader /> : 
                     <>
+                        {setIsConfettiVisible(true)}                      
                         <div className="thank-you">
                             <span>
                                 <h1>🥳 THANK YOU! 🥳</h1> 
